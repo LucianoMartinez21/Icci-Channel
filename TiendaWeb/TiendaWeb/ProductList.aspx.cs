2a08347542e96c043e37766ec05921b34748218d
Luciano Martinez <lucianoemartinezl@gmail.com>
2022-12-11 22:40:25 -0300

    GetProductos creado

--- TiendaWeb/TiendaWeb/ProductList.aspx.cs
+++ TiendaWeb/TiendaWeb/ProductList.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using TiendaWeb.Models;
+using System.Web.ModelBinding;
 
 namespace TiendaWeb
 {
@@ -13,6 +14,16 @@ namespace TiendaWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId) 
+        {
+            var _db = new TiendaWeb.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if(categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryId == categoryId);
+            }
+            return query;
         }
     }
 }
